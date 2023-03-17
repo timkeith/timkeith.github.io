6017effc8a07e97fbcbb82728befcd58ac1b0c0b
Tim Keith <tim@tkeith.com>
2023-03-16 17:47:36 -0700

    add flashcards

---
+++ flashcards/precache-manifest.cf0a7f31ab5ede6be60673e859c3075e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e941029adb01f8ba807e",
+    "url": "/static/css/main.0dc8e430.chunk.css"
+  },
+  {
+    "revision": "e941029adb01f8ba807e",
+    "url": "/static/js/main.2d2ade48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e5a32fdfe500797c11f5",
+    "url": "/static/css/2.9638d04d.chunk.css"
+  },
+  {
+    "revision": "e5a32fdfe500797c11f5",
+    "url": "/static/js/2.310bb672.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "683be8b3e5897c27db2504c52eb7ed77",
+    "url": "/index.html"
+  }
+];
